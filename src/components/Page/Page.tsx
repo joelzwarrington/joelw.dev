0897cb74de7a004545e03e4d2f1a502d91d624e0
Joel Warrington <joel.w@getjobber.com>
2021-11-04 02:42:52 -0600

    Implement navigation bar and basic routing  (#1)

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,10 +1,14 @@
 import React, { ReactNode } from "react";
 import styles from "./Page.module.css";
+import { Navigation } from "@/components/Navigation";
 
 export function Page({ children }: { children: ReactNode | ReactNode[] }) {
   return (
     <div className={styles.container}>
-      <div className={styles.page}>{children}</div>
+      <div className={styles.page}>
+        <Navigation />
+        <div className={styles.content}>{children}</div>
+      </div>
     </div>
   );
 }
