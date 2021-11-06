564317d56d99e0098d180dcf81c196ba20fb5b80
Joel Warrington <joelwarrington@gmail.com>
2021-11-06 05:26:57 -0600

    Updated layout and composition of the contact page (#7)

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Page } from "@/components/Page";
+import { Contact } from "@/features/Contact";
 
 export default function ContactPage() {
-  return <Page>Contact Page</Page>;
+  return (
+    <Page>
+      <Contact />
+    </Page>
+  );
 }
