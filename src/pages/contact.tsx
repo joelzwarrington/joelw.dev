bcdc0043ebee241afe0a415e324bbd0a53c81bb9
Joel Warrington <joelwarrington@gmail.com>
2021-12-10 03:14:54 -0700

    added blog components and head to title pages (#12)

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Head from "next/head";
 import { Page } from "@/components/Page";
 import { Contact } from "@/features/Contact";
 
 export default function ContactPage() {
   return (
     <Page>
+      <Head>
+        <title>Contact | Joel Warrington</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Contact />
     </Page>
   );
